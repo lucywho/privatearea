168a0e31fd3312c3a817f5cb69c72d160d13722c
Lucy Toman <lucy@toman.me.uk>
2022-08-22 11:54:10 +0200

    initial setup and tailwind installed

---
+++ components/Header.js
@@ -0,0 +1,11 @@
+import Head from "next/head"
+
+export default function Header() {
+    return (
+        <Head>
+            <title>Private Area</title>
+            <meta name="description" content="tutorial on private areas" />
+            <link rel="icon" href="/accordion.ico" />
+        </Head>
+    )
+}
