e58f9844ce4e287cf5a8a2bab155c6bce92983ac
Lucy Toman <lucy@toman.me.uk>
2022-08-24 14:52:48 +0200

    adds markdown

---
+++ content.md
@@ -0,0 +1,10 @@
+# Testing title
+
+testing content
+
+-   testing list
+-   testing list
+
+🧡🧡🧡
+
+[testing a link](https://lucysco.de)
--- pages/members.js
+++ pages/members.js
@@ -1,8 +1,9 @@
 import Header from "components/Header"
 import { useRouter } from "next/router"
 import { useSession } from "next-auth/react"
+import { getMarkdown } from "lib/markdown"
 
-export default function Members() {
+export default function Members({ markdown }) {
     const router = useRouter()
     const { data: session, status } = useSession()
     const loading = status === "loading"
@@ -29,23 +30,23 @@ export default function Members() {
     return (
         <>
             <Header />
-            <div>
-                <div className="text-center ">
-                    <h1 className=" mt-20 font-extrabold text-2xl">
-                        Private Area
-                    </h1>
-
-                    <p className="mt-10">Thank you for being a member</p>
-
-                    <p className="mt-10">You now have access to:</p>
-
-                    <ol className="mt-10 list-inside list-decimal">
-                        <li>The lyrics book in PDF</li>
-                        <li>Exclusive 30% discount on the albums</li>
-                        <li>Exclusive access to preorders</li>
-                    </ol>
-                </div>
+            <div className="text-center ">
+                <h1 className=" mt-20 font-extrabold text-2xl">Private Area</h1>
+                <div
+                    className="markdown"
+                    dangerouslySetInnerHTML={{ __html: markdown }}
+                />
             </div>
         </>
     )
+}
+
+export async function getServerSideProps(context) {
+    const markdown = await getMarkdown()
+
+    return {
+        props: {
+            markdown,
+        },
+    }
 }
